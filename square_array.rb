61d858fee7555a19f34b1852b2fed748e79dd55a
Alicia Weenum <weenum.alicia@gmail.com>
2020-10-21 12:45:00 -0400

    commit tree de57447f402f36f9ac2830242dbdfa7d323955aa

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
+# def square_array(array)
+   #array.each do |i|
+    #i ** 2
+   # end# your code here
+#end
+
 def square_array(array)
-  # your code here
+  Enumerator.new do |y|
+    array.each { |e| y << e ** 2 }    
+  end
+  .take(array.length)
 end
